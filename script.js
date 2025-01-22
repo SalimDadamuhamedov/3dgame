cf432b3b44aa4767032381ae5de6eb9291f2c979
MSD <dadamuhames@gmail.com>
2025-01-22 14:03:43 +0500

    wd

--- script.js
+++ script.js
@@ -40,22 +40,17 @@ let shadowGenerator = new BABYLON.ShadowGenerator(1024, light);
 // ПЕРЕМЕННЫЕ СТАТУС ИГРЫ
 let inMove = false;
 let gameOver = false;
-let score = 0;
 let nowscore = 0;
 let loaded = false;
-let onShop = false;
 
 //ИЗВЛЕЧЕНИЕ
-let scoreInfor = document.getElementById("scoreInfor");
 let gameOverScreen = document.getElementById("gameOver");
 let restart = document.getElementById("restart");
 let nowScore = document.getElementById("nowScore");
 let bestscoreinfo = document.getElementById("bestscoreinfo");
 let loadscreen = document.getElementById("loadscreen");
-let shopIcon = document.getElementById("shop-icon");
-let backIcon = document.getElementById("back-icon");
-let shopScreen = document.getElementById("shop-screen");
-let priceBlock = document.getElementById("price-block");
+let score = document.getElementById('score');
+let scoren = document.getElementById("scoren");
 //ИГРОВЫЕ ОБЪЕКТЫ
 let platformArray = [];
 
@@ -88,33 +83,7 @@ const creatPlatform = (zPos) => {
     platformArray.push(platform);
 };
 
-let coinArray = [];
-let colArray = [];
-let coin = null;
-const createCoin = (pos) => {
-    let collissionMesh = new BABYLON.MeshBuilder.CreateBox(
-        "colBox",
-        { size: 1 },
-        scene
-    );
-    collissionMesh.hasVertexAlpha = true;
-    collissionMesh.visibility = 0;
-    collissionMesh.position = pos;
-    BABYLON.SceneLoader.ImportMesh(
-        null,
-        "assets/models/",
-        "coin.glb",
-        scene,
-        (meshArray) => {
-            coin = meshArray[0];
-            shadowGenerator.addShadowCaster(coin);
-            coin.receiveShadows = true;
-            coin.parent = collissionMesh;
-            coinArray.push(coin);
-        }
-    );
-    colArray.push(collissionMesh);
-};
+
 
 let coneArray = [];
 const createCone = (xPos, zPos) => {
@@ -153,7 +122,6 @@ const createConeRow =  (zPos) => {
     lastRand = rand;
     for (let i = 0; i < 3; i++) {
         if (i === rand) {
-            createCoin(new BABYLON.Vector3(i * 2 - 2, 1, zPos + 5));
             pointArray.push(new BABYLON.Vector3(i * 2 - 2, 0.2, zPos + 5));
             continue;
         }
@@ -187,7 +155,7 @@ const createCar = (needP = true) => {
     BABYLON.SceneLoader.ImportMesh(
         "",
         "assets/models/",
-        "car0.glb",
+        "car1.glb",
         scene,
         (meshes) => {
             let car = meshes[0];
@@ -196,8 +164,6 @@ const createCar = (needP = true) => {
             car.position = new BABYLON.Vector3(0, -0.5, -0.3);
             car.rotation = new BABYLON.Vector3(0, Math.PI * 2, 0);
             shadowGenerator.addShadowCaster(car);
-            if (!onShop && coinArray.length > 8) shopIcon.style.display = 'block';
-            if (onShop) {backIcon.style.display = 'block'}
         }
     );
 
@@ -206,28 +172,6 @@ const createCar = (needP = true) => {
 };
 
 //ФУНКЦИИ ИГРЫ
-let demoArray = [];
-let demoTimer;
-const createDemoObjects = () => {
-    let box = createCar(false);
-    box.position = new BABYLON.Vector3(0, 6, 0);
-    box.scaling = new BABYLON.Vector3(1.5, 1.5, 1.5);
-    box.rotation.y += BABYLON.Tools.ToRadians(135);
-    demoArray.push(box);
-    demoTimer = setInterval(() => {
-        box.rotate(
-            new BABYLON.Vector3(0, 1, 0),
-            Math.PI * (engine.getDeltaTime() / 1000),
-            BABYLON.Space.WORLD
-        );
-    }, 20);
-};
-
-const deleteDemoObjects = () => {
-    box.dispose();
-    clearArray(demoArray);
-    clearInterval(demoTimer);
-};
 
 const saveBestScore = () => {
     window.localStorage.setItem("Bestscore", nowscore);
@@ -240,6 +184,7 @@ const loadBestScore = () => {
 };
 
 const setGameOverScreen = () => {
+    scoren.style.display = 'none';
     gameOverScreen.style.display = "block";
     let Bestscore = loadBestScore();
     if (nowscore > Bestscore) {
@@ -250,18 +195,6 @@ const setGameOverScreen = () => {
     }
     bestscoreinfo.textContent = `BEST: ${Bestscore}`;
     nowScore.textContent = `NOW: ${nowscore}`;
-};
-
-const saveCoinScore = () => {
-    window.localStorage.setItem("score", score);
-};
-
-const loadCoinScore = () => {
-    score = window.localStorage.getItem("score");
-    scoreInfor.textContent = score;
-    if (window.localStorage.getItem("score") === null) {
-        scoreInfor.textContent = 0;
-    }
 };
 
 const newRoadBlock = (zPos) => {
@@ -277,9 +210,7 @@ const clearArray = (array, isMesh = true) => {
 const deleteGameObjects = () => {
     box.dispose();
     clearArray(platformArray);
-    clearArray(coinArray);
     clearArray(coneArray);
-    clearArray(colArray);
     clearArray(pointArray, false);
 };
 
@@ -294,6 +225,7 @@ const createGameObjects =  () => {
 
 const restartGame = () => {
     gameOverScreen.style.display = "none";
+    scoren.style.display = 'flex';
     bestscoreinfo.style.color = "gray";
     nowScore.style.color = "gray";
     deleteGameObjects();
@@ -303,7 +235,7 @@ const restartGame = () => {
         skybox.position = new BABYLON.Vector3.Zero();
         nowscore = 0;
         speed = 20;
-        shopIcon.style.opacity = "1";
+        score.textContent = nowscore;
 };
 //ОБРАБОТЧИКИ
 
@@ -311,14 +243,10 @@ window.addEventListener("touchend", (event) => {
     if (loaded) {
         if (
             !inMove &&
-            !gameOver &&
-            event.changedTouches[0].screenY > 0.3 * window.screen.height &&
-            !onShop
+            !gameOver
         ) {
             box.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(0, 0, speed));
             inMove = true;
-            shopIcon.style.opacity = "0.3";
-            shopIcon.style
         } else if (inMove && !gameOver) {
             let touch = event.changedTouches[0];
             let x = touch.clientX;
@@ -336,14 +264,22 @@ window.addEventListener("touchend", (event) => {
                 }
             }
         }
+        if (inMove && !gameOver) {
+             box.physicsImpostor.setLinearVelocity(
+               new BABYLON.Vector3(0, 0, speed)
+             );
+             box.physicsImpostor.setAngularVelocity(
+               new BABYLON.Vector3(0, 0, 0)
+             );
+        }
     }
+    
 });
 
 window.addEventListener("keydown", (e) => {
-    if (e.key === " " && !inMove && !gameOver && loaded && !onShop) {
+    if (e.key === " " && !inMove && !gameOver && loaded) {
         box.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(0, 0, speed));
         inMove = true;
-        shopIcon.style.opacity = "0.3";
     }
     if (e.key === "a" && inMove && !gameOver) {
         e.preventDefault();
@@ -375,31 +311,10 @@ window.addEventListener("load", () => {
     }, 2000);
 });
 
-shopIcon.addEventListener("click", () => {
-    if (!inMove && !gameOver) {
-        onShop = true;
-        deleteGameObjects();
-        createDemoObjects();
-        shopIcon.style.display = "none";
-        priceBlock.style.display = "block";
-        shopScreen.style.display = "flex";
-    }
-});
-
-backIcon.addEventListener("click", () => {
-    deleteDemoObjects();
-    createGameObjects();
-    onShop = false;
-    backIcon.style.display = "none";
-    priceBlock.style.display = "none";
-    shopScreen.style.display = "none";
-});
-
 restart.addEventListener("click", restartGame);
 
-loadCoinScore();
 scene.registerBeforeRender(() => {
-    if (!gameOver && !onShop) { box.physicsImpostor.setAngularVelocity(new BABYLON.Vector3.Zero()); }
+    if (inMove && !gameOver) box.physicsImpostor.setAngularVelocity(new BABYLON.Vector3(0, 0, 0));
     if (box.getAbsolutePosition().z > 420)
         skybox.position.z = box.getAbsolutePosition().z - 420;
     light.position.z = box.getAbsolutePosition().z;
@@ -419,22 +334,12 @@ scene.registerBeforeRender(() => {
             setGameOverScreen();
         }
     }
-    for (let i = 0; i < colArray.length; i++) {
-        if (!onShop && box.intersectsMesh(colArray[i])) {
-            coinArray[i].dispose();
-            coinArray.slice(i, 1);
-            colArray[i].dispose();
-            colArray.splice(i, 1);
-            if (!gameOver) score++;
-            scoreInfor.textContent = score;
-            saveCoinScore();
-        }
-    }
 
     for (let i = 0; i < pointArray.length; i++) {
         if (box.intersectsPoint(pointArray[i])) {
             pointArray.splice(i, 1);
             if (!gameOver) nowscore++;
+            score.textContent = nowscore;
             if (pointArray.length < 9 && !gameOver) {
                 newRoadBlock(pointArray[pointArray.length - 1].z + 7);
             }
